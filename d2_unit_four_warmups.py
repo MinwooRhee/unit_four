624125b48d7bd4a90fd0d7082aae24bc19914d1f
Min Woo <minwoo@iMac5.local>
2018-10-04 12:22:09 -0400

    if - else statement

--- d2_unit_four_warmups.py
+++ d2_unit_four_warmups.py
@@ -1,5 +1,7 @@
 
 voting_age = 18
 age = int(input("What is your age?: "))
-if voting_age > age:
+if age >= voting_age:
+    print("you are old enough to vote")
+else:
     print("You are not old enough to vote")
