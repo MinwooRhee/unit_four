7f58b29e03eae58c54f3a6017548d2417bc32d93
Min Woo <minwoo@iMac5.local>
2018-10-08 14:29:00 -0400

    if, elif, and else

---
+++ d4_unit_four_warmups.py
@@ -0,0 +1,9 @@
+
+age = float(input("what is your age?: "))
+
+if 2 >= age:
+    print("You can get in the theatre free of charge.")
+elif 12 >= age:
+    print("Your ticket costs $8.")
+else:
+    print("Your ticket costs $10.")
